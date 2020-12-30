b2cbb8dd292e650a8f6b5fef8f413037586c0521
Kachelmann <kedouard@88e9fe671058.ant.amazon.com>
2020-12-30 15:44:54 -0500

    CDK iot and c9 adds

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rasp-aws-iot-temperature
+
+## Architecture
+
+<img src="https://github.com/eddie2070/aws-consoleblocker/blob/main/img/Temperature-sensor.png?raw=true"/>
--- cdk/rasptemp-cdk/lib/rasptemp_service.js
+++ cdk/rasptemp-cdk/lib/rasptemp_service.js
@@ -5,6 +5,7 @@ const timestream = require("@aws-cdk/aws-timestream");
 //const s3 = require("@aws-cdk/aws-s3");
 const iam = require("@aws-cdk/aws-iam");
 const iot = require("@aws-cdk/aws-iot");
+const cloud9 = require("@aws-cdk/aws-cloud9");
 
 class RaspService extends core.Construct {
   constructor(scope, id) {
@@ -25,7 +26,7 @@ class RaspService extends core.Construct {
       retentionProperties: {
         "MemoryStoreRetentionPeriodInHours": "24",
         "MagneticStoreRetentionPeriodInDays": "2555"
-    }
+      }
     })
 
     timestreamtable.node.addDependency(timestreamdb);
@@ -33,11 +34,11 @@ class RaspService extends core.Construct {
     const lambdamomentlayer = new lambda.LayerVersion(this, "moment-tz-layer", {
       compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
       code: lambda.Code.asset("resources"),
-/*       content: {
-        s3Bucket: "20180302pdfparse",
-        s3Key: "moment-layer.zip"
-    }, */
-    description: 'A layer with moment timezone',
+      /*       content: {
+              s3Bucket: "20180302pdfparse",
+              s3Key: "moment-layer.zip"
+          }, */
+      description: 'A layer with moment timezone',
     })
 
     const handler = new lambda.Function(this, "requestTemperatureFromRasp", {
@@ -67,23 +68,88 @@ class RaspService extends core.Construct {
         "timestream:CancelQuery",
         "timestream:ListDatabases",
         "timestream:SelectValues"
-    ],
-      resources: [ 'arn:aws:timestream:*:753451452012:database/RaspTemperature/table/location1' ]
+      ],
+      resources: ['arn:aws:timestream:*:753451452012:database/RaspTemperature/table/location1']
     }));
 
     const iotrule = new iot.CfnTopicRule(this, 'rasptemprule', {
       ruleName: 'rasptemprule',
       topicRulePayload: {
-        actions : [
+        actions: [
           {
-          lambda: { functionArn: handler.functionArn } // replace by timestream when available in CDK/CFN
-      }
+            lambda: { functionArn: handler.functionArn } // replace by timestream when available in CDK/CFN
+          }
         ],
-        ruleDisabled : true,
-        sql : "SELECT temperature as temp, humidity, getdate() as curdate FROM 'sensor/location1/temp1'"
+        ruleDisabled: true,
+        sql: "SELECT temperature as temp, humidity, getdate() as curdate FROM 'sensor/location1/temp1'"
       }
     })
-    
+
+    const iotc9sensor = new iot.CfnThing(this, 'cloud9sensor', {
+      thingName: 'cloud9sensor'
+    })
+
+    const iotsensorpolicy = new iot.CfnPolicy(this, 'cloud9sensor-policy', {
+      policyName: 'cloud9sensor-policy',
+      policyDocument: {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Action": [
+                "iot:Publish",
+                "iot:Receive"
+              ],
+              "Resource": [
+                "arn:aws:iot:us-east-1:753451452012:topic/sdk/test/java",
+                "arn:aws:iot:us-east-1:753451452012:topic/sdk/test/Python",
+                "arn:aws:iot:us-east-1:753451452012:topic/sensor/location1/temp1",
+                "arn:aws:iot:us-east-1:753451452012:topic/sensor/location1/temp1/sound",
+                "arn:aws:iot:us-east-1:753451452012:topic/topic_1",
+                "arn:aws:iot:us-east-1:753451452012:topic/topic_2"
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "iot:Subscribe"
+              ],
+              "Resource": [
+                "arn:aws:iot:us-east-1:753451452012:topicfilter/sdk/test/java",
+                "arn:aws:iot:us-east-1:753451452012:topicfilter/sdk/test/Python",
+                "arn:aws:iot:us-east-1:753451452012:topicfilter/sensor/location1/temp1",
+                "arn:aws:iot:us-east-1:753451452012:topicfilter/sensor/location1/temp1/sound",
+                "arn:aws:iot:us-east-1:753451452012:topicfilter/topic_1",
+                "arn:aws:iot:us-east-1:753451452012:topicfilter/topic_2"
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "iot:Connect"
+              ],
+              "Resource": [
+                "arn:aws:iot:us-east-1:753451452012:client/sdk-java",
+                "arn:aws:iot:us-east-1:753451452012:client/basicPubSub",
+                "arn:aws:iot:us-east-1:753451452012:client/sdk-nodejs-*"
+              ]
+            }
+          ]
+        }
+    })
+
+    const c9sensorenv = new cloud9.CfnEnvironmentEC2(this, 'cloud9env', {
+      automaticStopTimeMinutes : 15,
+      description: "cloud9 sensor env",
+      instanceType: "t3.micro",
+      name: "c9-temp-sensor",
+      subnetId: "subnet-013519962bcda2cba"
+    })
+
+    // print the Cloud9 IDE URL in the output
+    //new cdk.CfnOutput(this, 'URL', { value: c9sensorenv.ideUrl });
+
+
   }
 }
 
